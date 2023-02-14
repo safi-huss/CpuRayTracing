b6a21541f1e3bff0ce9d2a86e65df7e94cd22663
safi-huss <safiullah.hussaini@gmail.com>
2023-02-14 12:00:14 +0500

    New branch for signaloid

--- src/cpuraytracing.cpp
+++ src/cpuraytracing.cpp
@@ -1,6 +1,33 @@
+#ifdef _WIN32
 #include "app.h"
-
 #include "cpuraytracing.h"
+#elif defined(__linux)
+#define __STDC_LIB_EXT1__
+#define _USE_MATH_DEFINES
+#include <iostream>
+#include <string>
+#include <memory>
+#include <chrono>
+#include <vector>
+#include <sstream>
+#include <fstream>
+#include <algorithm>
+#include <iomanip>
+#include <cmath>
+
+#include <float.h>
+
+#define GLM_FORCE_ALIGNED_GENTYPES
+#define GLM_EXT_INCLUDED
+#define GLM_ENABLE_EXPERIMENTAL
+#include <../glm/glm.hpp>
+#include <../glm/ext.hpp>
+#include <../glm/gtx/component_wise.hpp>
+
+#include "app.h"
+#include "cpuraytracing.h"
+
+#endif
 
 const float LOCAL_EPSILON = 0.00004f;
 
