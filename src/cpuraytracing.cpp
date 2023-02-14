7ba7ef94dec35007ee180c43159beab6b3d80c46
safi-huss <safiullah.hussaini@gmail.com>
2023-02-14 14:14:51 +0500

    Arrrrgh

--- src/cpuraytracing.cpp
+++ src/cpuraytracing.cpp
@@ -20,7 +20,7 @@
 #define GLM_FORCE_ALIGNED_GENTYPES
 #define GLM_EXT_INCLUDED
 #define GLM_ENABLE_EXPERIMENTAL
-#include "glm]\glm.hpp"
+#include "glm\glm.hpp"
 #include "glm/ext.hpp"
 #include "glm/gtx/component_wise.hpp"
 
