35c4c33e649137d5cdf28a69a8fb0dc5223a3a00
safi-huss <safiullah.hussaini@gmail.com>
2023-02-14 12:05:13 +0500

    fixing glm includes

--- src/cpuraytracing.cpp
+++ src/cpuraytracing.cpp
@@ -20,9 +20,9 @@
 #define GLM_FORCE_ALIGNED_GENTYPES
 #define GLM_EXT_INCLUDED
 #define GLM_ENABLE_EXPERIMENTAL
-#include "../glm/glm.hpp"
-#include "../glm/ext.hpp"
-#include "../glm/gtx/component_wise.hpp"
+#include "../glm/glm/glm.hpp"
+#include "../glm/glm/ext.hpp"
+#include "../glm/glm/gtx/component_wise.hpp"
 
 #include "app.h"
 #include "cpuraytracing.h"
