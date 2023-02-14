5b5cdadff9a933078576b18c37ec50e22a11b604
safi-huss <safiullah.hussaini@gmail.com>
2023-02-14 21:06:55 +0500

    WIN32 codebranch

--- src/app.h
+++ src/app.h
@@ -15,9 +15,18 @@
 
 #include <float.h>
 
+#ifdef _WIN32
 #define GLM_FORCE_ALIGNED_GENTYPES
 #define GLM_EXT_INCLUDED
 #define GLM_ENABLE_EXPERIMENTAL
 #include <glm/glm.hpp>
 #include <glm/ext.hpp>
 #include <glm/gtx/component_wise.hpp>
+#else
+#define GLM_FORCE_ALIGNED_GENTYPES
+#define GLM_EXT_INCLUDED
+#define GLM_ENABLE_EXPERIMENTAL
+#include "glm/glm.hpp"
+#include "glm/ext.hpp"
+#include "glm/gtx/component_wise.hpp"
+#endif
